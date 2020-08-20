09769c8306d362e013df2ba9b88f5d4605b22054
Gretchen Harris <gretchenh77@yahoo.com>
2020-08-20 18:26:37 -0400

    Routing in React

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
 import "./App.css";
 import Welcome from "./components/welcome/Welcome";
 import Clock from "./components/clock/Clock";
 import Contact from "./components/contact/Contact";
+import Navigation from "./components/navigation/Navigation";
+import Error from "./components/error/Error";
 
 function App() {
   return (
     <div className="App">
-      <Welcome name="Gretchen" />
-      <Clock />
-      <Contact />
+      <Navigation />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={(props) => <Welcome {...props} name="Gretchen" />}
+        />
+        <Route
+          exact
+          path="/Welcome/:name"
+          render={(props) => (
+            <Welcome {...props} name={props.match.params.name} />
+          )}
+        />
+        <Route path="/clock" component={Clock} />
+        <Route path="/contact" component={Contact} />
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/error/Error.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Error(props) {
+  return <div className="Error">Page Not Found!</div>;
+}
+
+export default Error;
